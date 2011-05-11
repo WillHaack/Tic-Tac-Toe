b4b45367c8bff5b9522ccfc7f79f200c6f268162
WillHaack <Will@.(none)>
2011-05-11 09:10:14 -0400

    makes a board with an 3 x 3 int[][]

---
+++ Board.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+/**
+ *
+ * @author Will
+ */
+public class Board {
+    private int[][] gameboard; //0 is empty, 1 is x, 2 is 0
+    
+    public Board(){
+        gameboard = new int[3][3];
+    }
+    
+    
+}
