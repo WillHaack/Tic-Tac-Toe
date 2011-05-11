48c9bad0d37283b402b3fdcbca3938451d11d95f
WillHaack <Will@.(none)>
2011-05-11 09:00:23 -0400

    main class. makes a new board

---
+++ TicTacToe.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+/**
+ *
+ * @author Will
+ */
+public class TicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Board gameboard = new Board();// TODO code application logic here
+    }
+}
