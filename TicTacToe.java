323dcdf92f4007bb2e8059a10cf918546ff9960e
WillHaack <Will@.(none)>
2011-05-11 11:12:39 -0400

    changed some of the main methods

--- TicTacToe.java
+++ TicTacToe.java
@@ -14,6 +14,10 @@ public class TicTacToe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Board gameboard = new Board();// TODO code application logic here
+        Board gameboard = new Board();
+        gameboard.makeMove(2, 1, 1);
+        gameboard.makeMove(1, 1, 1);
+        gameboard.makeMove(0, 1, 1);
+        gameboard.printBoard();// TODO code application logic here
     }
 }
