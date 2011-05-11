9edff654d03584364d725f3052dd902bbc6d86ef
WillHaack <Will@.(none)>
2011-05-11 10:00:32 -0400

    added canmove(int x, int y) makemove(int x, int y, int n) and printboard()

--- Board.java
+++ Board.java
@@ -9,11 +9,37 @@ package tictactoe;
  * @author Will
  */
 public class Board {
+
     private int[][] gameboard; //0 is empty, 1 is x, 2 is 0
-    
-    public Board(){
+
+    public Board() {
         gameboard = new int[3][3];
     }
-    
-    
+
+    public boolean makeMove(int x, int y, int n) {
+        if (gameboard[x][y] == 0) {
+            gameboard[x][y] = n;
+            return true;
+        }
+        return false;
+    }
+
+    public void printBoard() {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (gameboard[r][c] == 0) {
+                    System.out.print("  ");
+                } else if (gameboard[r][c] == 1) {
+                    System.out.print("X ");
+                } else {
+                    System.out.print("O ");
+                }
+
+            }
+        }
+    }
+
+    public boolean canMove(int x, int y) {
+        return gameboard[x][y] == 0;
+    }
 }
